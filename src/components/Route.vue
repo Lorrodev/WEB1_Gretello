a4eb9306899706009652710f838ec8f8a4f3ddf2
Marc Zuber <marc.zuber@gmail.com>
2021-11-17 14:15:17 +0100

    Added Vue Proj

---
+++ src/components/Route.vue
@@ -0,0 +1,15 @@
+<template>
+    This is a test for Route :)<br>
+    <br>
+    {{ this.name }}
+</template>
+
+<script>
+export default {
+  name: 'Route',
+  props: {
+    name: String,
+    description: String
+  }
+}
+</script>
---
+++ src/views/Route.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="about">
+    <h1 id="testh1">This is a Route page</h1>
+    <Route name="Test Route"/>
+  </div>
+</template>
+
+<script>
+import Route from '@/components/Route.vue'
+
+    export default {
+        name: 'RouteView',
+        components: {
+            Route
+        }
+    }
+</script>
